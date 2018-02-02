e089e9de0d5f32356eaaa4d356d54f66c7a786db
leo <leonard.becker@protonmail.com>
2018-02-02 22:43:25 +0100

    parse citites from FEN string

--- CannonBot.hs
+++ CannonBot.hs
@@ -6,8 +6,27 @@ import Util
 getMove :: String -> String
 listMoves :: String -> String
 
-data Field = BlackPlayer | WhitePlayer | BlackCity | WhiteCity | Empty
+--data Player = Player{row::Int, col::Int, white::Bool}
+data City = City {row :: Int, col :: Int, white :: Bool}
+--data Field = Player | City | Empty
 
+instance Show City where
+    show (City r c True) = "W{r:" ++ show r ++ ",c:" ++ show c ++ "}"
+    show (City r c False) = "B{r:" ++ show r ++ ",c:" ++ show c ++ "}"
+
+--getWhitePlayers :: String -> [Player]
+--getBlackPlayers :: String -> [Player]
+
+stringToCity :: String -> Int -> City
+stringToCity (l:t) c = let w = c == 0 in (City ((digitToInt l) + 1) c w)
+
+getCities :: String -> [City]
+getCities fen = let l = (splitOn "/" fen) in (stringToCity (head l) 0):(stringToCity (last l) 10):[]
+ 
+getWhiteCity :: [Char] -> City
+getWhiteCity fen = City (digitToInt (head (head (splitOn "/" fen)))) 0 False
+
+--getBlackCity :: String -> City
 
 -- Input Format: 4W5/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w///b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/7B2 w
 -- Output Format: a0-b0
