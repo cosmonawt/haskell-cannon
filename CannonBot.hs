9264df1dbd35b48a2665d987fc36187df13f3f0d
leo <leonard.becker@protonmail.com>
2018-02-04 00:54:18 +0100

    completely new approach, work with lists only now

--- CannonBot.hs
+++ CannonBot.hs
@@ -6,26 +6,21 @@ import Util
 getMove :: String -> String
 listMoves :: String -> String
 
-data Player = Player {pc :: Int, pr :: Int, pw :: Bool}
-data City = City {cc :: Int, cr :: Int, cw :: Bool}
+-- UTILITIES
 
-instance Show City where
-    show (City cc cr True) = "W{" ++ id (coordsString cc cr) ++ "}"
-    show (City cc cr False) = "B{" ++ id (coordsString cc cr) ++ "}"
-
-instance Show Player where
-    show (Player pr pc True) = "w{" ++ id (coordsString pr pc) ++ "}"
-    show (Player pr pc False) = "b{" ++ id (coordsString pr pc) ++ "}"
-
--- Utilities
-coordsString :: Int -> Int -> String
-coordsString c r = (colNumToChar c) : intToDigit r : []
-
+-- Char representation of column number
 colNumToChar :: Int -> Char
-colNumToChar i = "abcdefghij"!!i
+colNumToChar i = ['a'..'j']!!i
+
+-- Numbers [2..9] as chars
+numbers29 :: String
+numbers29 = ['2'..'9']
 
 isPlayer :: Char -> Bool
 isPlayer c = elem c "wb"
+
+isCity :: Char -> Bool
+isCity c = elem c "WB"
 
 findAfter :: (Char -> Bool) -> String -> Int -> Int
 findAfter f (h:[]) n = if f h then n else -1
@@ -34,34 +29,35 @@ findAfter f (h:r) n = if f h then n else (findAfter (f) r (n + 1))
 findFirstPlayer :: String -> Int
 findFirstPlayer s = (findAfter isPlayer s 0)
 
--- Logic
+-- Sum up numerical chars in list of chars
+charSum :: [Char] -> Int
+charSum s = foldr (+) 0 (map digitToInt s)
 
--- TODO: Fix 4W5 vs 1111W5 Format issue
--- TODO: Fix row 0 -> White issue (?)
--- 4W5
-stringToCity :: String -> Int -> City
-stringToCity (l:t) c = let w = c == 0 in (City ((digitToInt l) + 1) c w)
+-- Replace chars with numerical >1 with list of numbers of equal sum
+convertToNumbers :: Char -> [Char]
+convertToNumbers c = if (elem c numbers29) then (take (digitToInt c) ['1','1'..]) else [c]
 
--- 4W5/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w///b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/7B2
-getCitiesList :: String -> [City]
-getCitiesList fen = let (lh:lr) = (splitOn "/" fen) in (stringToCity lh 9):(stringToCity (last lr) 0) : []
+-- Join List of Strings into one String
+stringsToString :: [String] -> String
+stringsToString ([]) = []
+stringsToString (h:r) = h ++ stringsToString r
 
-getPlayerStrings :: String -> [String]
-getPlayerStrings fen = let (lh:lr) = (splitOn "/" fen) in take 8 lr
+-- LOGIC
 
--- w 1w1w1w1w1w
-findIndexPlayers :: Char -> String -> [String]
-findIndexPlayers c row = (splitOn [c] row)
+-- Expand line with numbers >1 into line containing only 1s (1w3 -> 1w111)
+expandLine :: [Char] -> String
+expandLine s = stringsToString (map convertToNumbers s)
 
--- getPlayerOffsets :: [Char] -> [Int] -> [Int]
--- getPlayerOffsets ('':r) [] = [0] ++ getPlayerOffsets r 1
--- getPlayerOffsets (h:'':[]) l = l
--- getPlayerOffsets (h:r) l = l ++ [(last l) + h]
+-- Turn FEN String into list of strings (one per row)
+getRows :: String -> [String]
+getRows s = splitOn "/" s
 
--- 1w1w1w1w1w Row
---getPlayers :: String -> Int -> [Player]
---getPlayers row rc = 
+--expandRows :: [String] -> [String]
+--expandRows l = map ()
 
+-- Split list of row strings into list of list of strings (["ab","cd"] -> [["","a","b"]["","c","d"]])
+splitRows :: [String] -> [[String]]
+splitRows s = map (splitOn "") s
 
 
 -- Input Format: 4W5/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w///b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/7B2 w
