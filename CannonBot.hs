99ced6bf449e318984922ee5295ec3fced875094
leo <leonard.becker@protonmail.com>
2018-02-03 17:26:48 +0100

    add players type, find first player in string

--- CannonBot.hs
+++ CannonBot.hs
@@ -6,27 +6,63 @@ import Util
 getMove :: String -> String
 listMoves :: String -> String
 
---data Player = Player{row::Int, col::Int, white::Bool}
-data City = City {row :: Int, col :: Int, white :: Bool}
---data Field = Player | City | Empty
+data Player = Player {pc :: Int, pr :: Int, pw :: Bool}
+data City = City {cc :: Int, cr :: Int, cw :: Bool}
 
 instance Show City where
-    show (City r c True) = "W{r:" ++ show r ++ ",c:" ++ show c ++ "}"
-    show (City r c False) = "B{r:" ++ show r ++ ",c:" ++ show c ++ "}"
+    show (City cc cr True) = "W{" ++ id (coordsString cc cr) ++ "}"
+    show (City cc cr False) = "B{" ++ id (coordsString cc cr) ++ "}"
 
---getWhitePlayers :: String -> [Player]
---getBlackPlayers :: String -> [Player]
+instance Show Player where
+    show (Player pr pc True) = "w{" ++ id (coordsString pr pc) ++ "}"
+    show (Player pr pc False) = "b{" ++ id (coordsString pr pc) ++ "}"
 
+-- Utilities
+coordsString :: Int -> Int -> String
+coordsString c r = (colNumToChar c) : intToDigit r : []
+
+colNumToChar :: Int -> Char
+colNumToChar i = "abcdefghij"!!i
+
+isPlayer :: Char -> Bool
+isPlayer c = elem c "wb"
+
+findAfter :: (Char -> Bool) -> String -> Int -> Int
+findAfter f (h:[]) n = if f h then n else -1
+findAfter f (h:r) n = if f h then n else (findAfter (f) r (n + 1))
+
+findFirstPlayer :: String -> Int
+findFirstPlayer s = (findAfter isPlayer s 0)
+
+-- Logic
+
+-- TODO: Fix 4W5 vs 1111W5 Format issue
+-- TODO: Fix row 0 -> White issue (?)
+-- 4W5
 stringToCity :: String -> Int -> City
 stringToCity (l:t) c = let w = c == 0 in (City ((digitToInt l) + 1) c w)
 
-getCities :: String -> [City]
-getCities fen = let l = (splitOn "/" fen) in (stringToCity (head l) 0):(stringToCity (last l) 10):[]
- 
-getWhiteCity :: [Char] -> City
-getWhiteCity fen = City (digitToInt (head (head (splitOn "/" fen)))) 0 False
+-- 4W5/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w///b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/7B2
+getCitiesList :: String -> [City]
+getCitiesList fen = let (lh:lr) = (splitOn "/" fen) in (stringToCity lh 9):(stringToCity (last lr) 0) : []
 
---getBlackCity :: String -> City
+getPlayerStrings :: String -> [String]
+getPlayerStrings fen = let (lh:lr) = (splitOn "/" fen) in take 8 lr
+
+-- w 1w1w1w1w1w
+findIndexPlayers :: Char -> String -> [String]
+findIndexPlayers c row = (splitOn [c] row)
+
+-- getPlayerOffsets :: [Char] -> [Int] -> [Int]
+-- getPlayerOffsets ('':r) [] = [0] ++ getPlayerOffsets r 1
+-- getPlayerOffsets (h:'':[]) l = l
+-- getPlayerOffsets (h:r) l = l ++ [(last l) + h]
+
+-- 1w1w1w1w1w Row
+--getPlayers :: String -> Int -> [Player]
+--getPlayers row rc = 
+
+
 
 -- Input Format: 4W5/1w1w1w1w1w/1w1w1w1w1w/1w1w1w1w1w///b1b1b1b1b1/b1b1b1b1b1/b1b1b1b1b1/7B2 w
 -- Output Format: a0-b0
