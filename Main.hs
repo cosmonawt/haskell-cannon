9369057246fa1ed9ee12ad2a539ffb29b5ad5747
leo <leonard.becker@protonmail.com>
2018-02-10 23:19:19 +0100

    format working

--- Main.hs
+++ Main.hs
@@ -7,6 +7,6 @@ import System.Environment
 
 main :: IO ()
 main = do 
-	args <- getArgs
-	let oneString = foldr (\x y -> if y == "" then x else x ++ " " ++ y) "" args in
-		putStrLn ( getMove oneString )
+    args <- getArgs
+    let oneString = foldr (\x y -> if y == "" then x else x ++ " " ++ y) "" args in
+        putStrLn ( getMove oneString )
